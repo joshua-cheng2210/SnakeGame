33a6fdbdaec0f9995e35b13f1a944470d6cdd384
chen7647 <chen7647@umn.edu>
2024-12-25 22:53:13 -0600

    changed file directories

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    base: './', // Ensure the base path is set correctly for GitHub Pages
     outDir: 'dist',
     rollupOptions: {
       input: {
